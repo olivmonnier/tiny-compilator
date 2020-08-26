2dd3fd9c41a224b97efeeee44026d449c1519656
Olivier Monnier <monnier@indigo-net.com>
2020-08-26 15:46:05 +0200

    refacto & some fix

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-var assert = require('chai').assert;
-var expect = require('chai').expect;
-var loadFiles = require('../lib/loadFiles');
-var loadContents = require('../lib/loadContents');
-var Compilator = require('../index');
+const path = require('path');
+const { assert, expect } = require('chai');
+const loadFiles = require('../lib/loadFiles');
+const loadContents = require('../lib/loadContents');
+const Compilator = require('../index');
 
 describe('Load Files', function () {
   it('return a list of files', function (done) {
-    var files = loadFiles('test');
+    const files = loadFiles('test');
 
     assert.isArray(files, 'Return an array');
     done();
@@ -15,21 +15,21 @@ describe('Load Files', function () {
 
 describe('Load content in folder', function () {
   it('return a list of objects', function (done) {
-    var contents = loadContents('test/fixtures/layouts');
+    const contents = loadContents('test/fixtures/layouts');
 
     assert.isObject(contents[0], 'Return a list of object');
     done();
   });
 
   it('return the filename foreach files', function (done) {
-    var contents = loadContents('test/fixtures/layouts');
+    const contents = loadContents('test/fixtures/layouts');
 
     expect(contents[0].name).to.equal('default');
     done();
   });
 
   it('return the string content foreach files', function (done) {
-    var contents = loadContents('test/fixtures/layouts');
+    const contents = loadContents('test/fixtures/layouts');
 
     assert.isString(contents[0].content, 'Return a string');
     expect(contents[0].content).to.equal('<h1>Hello World</h1>{{> body }}');
@@ -39,19 +39,19 @@ describe('Load content in folder', function () {
 
 describe('Compilation works', function () {
   it('return instance of Compilator', function (done) {
-    var c = new Compilator();
+    const c = new Compilator();
 
     assert.instanceOf(c, Compilator, 'Return an instance');
     done();
   });
 
   it('return html elements after run render method', function (done) {
-    var c = new Compilator({
-      helpers: 'test/fixtures/helpers',
+    const c = new Compilator({
+      helpers: path.join(process.cwd(), 'test/fixtures/helpers'),
       layouts: 'test/fixtures/layouts/**',
       partials: 'test/fixtures/partials/**'
     });
-    var page = c.render('test/fixtures/pages/test.hbs', {
+    const page = c.render('test/fixtures/pages/test.hbs', {
       title: 'Hello Everyone'
     });
 
